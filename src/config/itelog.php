5872ba5b16e0917b85eca006027817dc50c9c99a
zhaoyg <5575165@qq.com>
2021-08-31 11:26:25 +0800

    新增异常处理

--- src/config/itelog.php
+++ src/config/itelog.php
@@ -3,6 +3,8 @@
 return [
     //是否开启true false
     'logger' => 'true',
+    //异常开启true false
+    'exception' => 'true',
     //驱动 mongodb | file | mysql（后续完善）
     'driver' => 'mongodb',
     //driver是mongodb 时 需要填写表名
